8af7a64d3798aad7f02a2fec4181abf26f928d63
Marius Jantea <jantea.marius@yahoo.com>
2016-05-04 15:03:48 +0300

    I added hello world

--- MyStuffApp1/Program.cs
+++ MyStuffApp1/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace MyStuffApp1
+﻿using System;
+
+namespace MyStuffApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world!");
         }
     }
 }
