5b25dc380ebb2c2948e754bf96cc2b783079a4f5
Jantea Marius <jantea.marius@outlook.com>
2016-05-05 08:01:19 +0300

    Added Person class

--- MyStuffApp1/Program.cs
+++ MyStuffApp1/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
+            Person p1 = new Person { FirstName = "Andrei", LastName = "Andrei" };
             System.Console.WriteLine("Hello world from home location!");
+            System.Console.WriteLine($"{p1.LastName} {p1.FirstName}");
         }
     }
 }
